04043e10fd0944895d4c3d47c1411080d0fef500
YuanWuTou <18819258370@163.com>
2018-11-14 15:49:38 +0800

    some changes

--- spring-cloud-zuul/src/main/java/com/yuanwutou/cloud/SpringCloudZuulApplication.java
+++ spring-cloud-zuul/src/main/java/com/yuanwutou/cloud/SpringCloudZuulApplication.java
@@ -12,7 +12,7 @@ import javax.accessibility.AccessibleRelation;
 public class SpringCloudZuulApplication {
 
 	public static void main(String[] args) {
-		System.out.println("1eee23456677");
+		System.out.println("1eee234SS56677");
 		SpringApplication.run(SpringCloudZuulApplication.class, args);
 	}
 
