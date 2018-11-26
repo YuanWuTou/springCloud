66024bdf434f16e41cee24a715290ee37fb255bf
YuanWuTou <18819258370@163.com>
2018-11-26 15:50:44 +0800

    some changes

--- spring-cloud-config-client/src/main/java/com/yuanwutou/cloud/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/yuanwutou/cloud/SpringCloudConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.yuanwutou.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
