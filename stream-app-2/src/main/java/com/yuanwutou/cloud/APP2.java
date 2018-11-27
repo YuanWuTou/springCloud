3e4709abf0e6372b5eb953e51548df530c8a31d6
YuanWuTou <18819258370@163.com>
2018-11-27 17:55:25 +0800

    some changes

---
+++ stream-app-2/src/main/java/com/yuanwutou/cloud/APP2.java
@@ -0,0 +1,27 @@
+package com.yuanwutou.cloud;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.util.Date;
+
+@EnableBinding(value = {Processor.class})
+public class APP2 {
+
+    @Bean
+    @InboundChannelAdapter(value = Processor.OUTPUT,poller = @Poller(fixedDelay = "2000"))
+    public MessageSource<Date> timerMessageSource(){
+        return() -> new GenericMessage<>(new Date());
+    }
+
+    @StreamListener(Processor.INPUT)
+    public void receiveFromOutput(String payload){
+        System.out.println("Received : "+payload);
+    }
+}
---
+++ stream-app-2/src/main/java/com/yuanwutou/cloud/StreamApp2Application.java
@@ -0,0 +1,12 @@
+package com.yuanwutou.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApp2Application.class, args);
+	}
+}
